096a899e294e77a7bde77dee4beb127b1441dc35
Lim <3269989624@qq.com>
2024-12-20 21:59:43 +0800

    24.12.20 :tada:

---
+++ Vue2/vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h2>{{ name }}</h2>
+    <h2>{{ address }}</h2>
+    <button @click="tip($event,5)">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: '野鸡大学',
+      address: '湖北省',
+    }
+  },
+  methods: {
+    tip(e, n) {
+      console.log(e, n)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .demo{
+    background-color: orange;
+  }
+</style>
